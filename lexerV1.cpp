b5bd3863ec53f1864aa52bd3215d341269a931d2
juan72795 <linaresjuan727@csu.fullerton.edu>
2019-03-01 07:13:58 +0000

    new version of the lexer with the line to remove comments added

--- lexerV1.cpp
+++ lexerV1.cpp
@@ -2,38 +2,62 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <stdlib.h>
+#include <ctype.h>
 //pre processor directives necessary for file manipulation
 //since using namespace is not portable code we will be forced to 
 //to include std:: before every type of output manipulation
-//
+using namespace std;
+struct Tokens{
+	//attributes to describe each tiken we will be taking in
+	string nameOfToken;
+	string typeName;
+}
+
+void splitString(vector<string> vectorInfo){
+	//will not return anything because we will only be manipulating items within a vector
+	
+}
+
 int main(){
 	//include means of both reading and writing to a file 
 	//then include a file
-	std::cout << "What file would you wish to process in this current Directory\n" << std::endl;
+	cout << "What file would you wish to process in this current Directory\n" << endl;
 	
-	std::string filename; 
-	std::cin >> filename;
+	vector<Tokens> VectorOfTokens;
+	vector<string> TempVector;
+		
+	cout << "Specify the file you wish to manipulate: "
+	string filename; 
+	cin >> filename;
+
+	string commentTrash; // we will ignore comments in the lexical analysis
+
 	//create the data containers that
 	//will house the items that we findthrough the lexical analyzer
-	std::vector<std::string> arrayTokens;
         // this array of tokens 	
-	
-	std::string processLine; //l;ine that will be used to process
-				// tokens found in the text file
-	std::fstream inputFile(filename); 
+       		            // line that will be used to process
+		            // tokens found in the text file
+	fstream inputFile(filename); 
 	// this should take in a file that the user should specify
 	//we now have the file accessed and open,it is here where we 
 	//need to begin processing the file to do what wer want
 	// as a lexical analyzer
 	// make sure to close the file when finished processing
-	
+
 	//read in the file and find a wya to process the contents
 	// and seperate them
 	
-	while(InputFile != NULL){
+	while(inputFile != NULL){
 		//while input file is taking in some form of inpuit
 		//we will need t process teh data within the text file
-		std::getline(processLine, inputFile);
+		if(inputFile.peek('!')){
+			//this should remove any line that uses! at the beginning,
+			//it is a comment operator/indicator
+			getline(inputFile,commentTrash);
+		}
+		
+
 	
 	}
 	
