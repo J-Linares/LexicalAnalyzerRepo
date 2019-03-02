8e9463d8fb904917ec547e3a23ae938e960ac27f
juan72795 <linaresjuan727@csu.fullerton.edu>
2019-03-02 07:34:04 +0000

    file now prompts user for file input correctly, will also confirm that the file was taken in

--- lexerV1.cpp
+++ lexerV1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <string.h>
 #include <vector>
 #include <stdlib.h>
 #include <ctype.h>
@@ -16,12 +17,6 @@ struct Tokens
     string typeName;
 };
 
-void splitString(vector<string> vectorInfo)
-{
-    //will not return anything because we will only be manipulating items within a vector
-
-}
-
 int main()
 {
     //include means of both reading and writing to a file
@@ -35,10 +30,12 @@ int main()
     //temporary vectors will be used to split items apart  from the global
     // storage, at the end , the vector of tokens will house all split items
     // and their definitions
-
     cout << "Specify the file you wish to manipulate: ";
+
     string filename;
     cin >> filename;
+
+    ifstream inputFile(filename);
 
     string commentTrash; // we will ignore comments in the lexical analysis
     string codeEntry;
@@ -47,7 +44,11 @@ int main()
     // this array of tokens
     // line that will be used to process
     // tokens found in the text file
-    fstream inputFile(filename);
+    inputFile.open(filename.c_str());
+        if(inputFile.is_open())
+        {
+            cout << "we have located the File: " << filename << endl;
+        }
     // this should take in a file that the user should specify
     //we now have the file accessed and open,it is here where we
     //need to begin processing the file to do what wer want
@@ -66,6 +67,7 @@ int main()
             //this should remove any line that uses! at the beginning,
             //it is a comment operator/indicator
             getline(inputFile,commentTrash);
+            cout <<"we are skipping a comment line " << commentTrash << endl;
         }
         //take the data into the data containers we will use for manipulation
         GlobalStorage.push_back(codeEntry);
@@ -78,9 +80,18 @@ int main()
     {
         //we begin splitting strings right here
         string currentLexeme =	GlobalStorage[idx];
+        char lexemeArray[1024];
+        strncpy(lexemeArray, currentLexeme.c_str(), sizeof(lexemeArray));
+        lexemeArray[sizeof(lexemeArray) - 1] = 0;
+
         //a respective lexeme should now be held inside this vector of chars
         //ex:  n, u , m , 1 , ","
-        TempVector(currentLexeme.begin(), currentLexeme.end());
+
+        //use a for loop to pass array contents into the TempVector
+        for(int arrIdx = 0; arrIdx < sizeof(lexemeArray); arrIdx++){
+            TempVector.push_back(lexemeArray[arrIdx]);
+        }
+
         vector<char> word;
         for(int charIdx = 0; charIdx < TempVector.size(); charIdx++)
         {
@@ -97,12 +108,11 @@ int main()
                 {
                     word.push_back(TempVector[charIdx]);
                 }
-                VectorOfTokens.push_back(word);
-                VectorOfTokens.push_back(TempVector[charIdx]);
+              //  VectorOfTokens.push_back(TempVector[charIdx]);
             }
             word.push_back(TempVector[charIdx]);
         }
-        VectorofTokens.push_back(word);
+     //   VectorofTokens.push_back(word);
         word.clear();
     }
 
