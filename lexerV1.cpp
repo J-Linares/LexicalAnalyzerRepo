b1db9add6ef026720a20abc9e5b9b24f7dcffe25
juan72795 <linaresjuan727@csu.fullerton.edu>
2019-03-02 05:36:17 +0000

    made some changes to syntax so that it could compile, though more debugging is necessary

--- lexerV1.cpp
+++ lexerV1.cpp
@@ -5,96 +5,107 @@
 #include <stdlib.h>
 #include <ctype.h>
 //pre processor directives necessary for file manipulation
-//since using namespace is not portable code we will be forced to 
+//since using namespace is not portable code we will be forced to
 //to include std:: before every type of output manipulation
 using namespace std;
-struct Tokens{
-	//attributes to describe each tiken we will be taking in
-	string nameOfToken;
-	string typeName;
+
+struct Tokens
+{
+    //attributes to describe each tiken we will be taking in
+    string nameOfToken;
+    string typeName;
+};
+
+void splitString(vector<string> vectorInfo)
+{
+    //will not return anything because we will only be manipulating items within a vector
+
 }
 
-void splitString(vector<string> vectorInfo){
-	//will not return anything because we will only be manipulating items within a vector
-	
-}
+int main()
+{
+    //include means of both reading and writing to a file
+    //then include a file
+    cout << "What file would you wish to process in this current Directory\n" << endl;
 
-int main(){
-	//include means of both reading and writing to a file 
-	//then include a file
-	cout << "What file would you wish to process in this current Directory\n" << endl;
-	
-	vector<Tokens> VectorOfTokens;	//as we store them, we will classify them
-					//with their respective token/lexeme
-	vector<char> TempVector;	//will  house split items for the vector
-	vector<string> GlobalStorage;	//stores all strings for manipulation 
-	//temporary vectors will be used to split items apart  from the global
-	// storage, at the end , the vector of tokens will house all split items
-	// and their definitions 
+    vector<Tokens> VectorOfTokens;	//as we store them, we will classify them
+    //with their respective token/lexeme
+    vector<char> TempVector;	//will  house split items for the vector
+    vector<string> GlobalStorage;	//stores all strings for manipulation
+    //temporary vectors will be used to split items apart  from the global
+    // storage, at the end , the vector of tokens will house all split items
+    // and their definitions
 
-	cout << "Specify the file you wish to manipulate: "
-	string filename; 
-	cin >> filename;
+    cout << "Specify the file you wish to manipulate: ";
+    string filename;
+    cin >> filename;
 
-	string commentTrash; // we will ignore comments in the lexical analysis
-	string codeEntry;
-	//create the data containers that
-	//will house the items that we findthrough the lexical analyzer
-        // this array of tokens 	
-       		            // line that will be used to process
-		            // tokens found in the text file
-	fstream inputFile(filename); 
-	// this should take in a file that the user should specify
-	//we now have the file accessed and open,it is here where we 
-	//need to begin processing the file to do what wer want
-	// as a lexical analyzer
-	// make sure to close the file when finished processing
+    string commentTrash; // we will ignore comments in the lexical analysis
+    string codeEntry;
+    //create the data containers that
+    //will house the items that we findthrough the lexical analyzer
+    // this array of tokens
+    // line that will be used to process
+    // tokens found in the text file
+    fstream inputFile(filename);
+    // this should take in a file that the user should specify
+    //we now have the file accessed and open,it is here where we
+    //need to begin processing the file to do what wer want
+    // as a lexical analyzer
+    // make sure to close the file when finished processing
 
-	//read in the file and find a wya to process the contents
-	// and seperate them
-	
-	while(inputFile >> codeEntry){
-		//while input file is taking in some form of inpuit
-		//we will need t process teh data within the text file
-		if(inputFile.peek('!')){
-			//this should remove any line that uses! at the beginning,
-			//it is a comment operator/indicator
-			getline(inputFile,commentTrash);
-		}
-	//take the data into the data containers we will use for manipulation	
-		GlobalStorage.push_back(codeEntry);	
+    //read in the file and find a wya to process the contents
+    // and seperate them
 
-	}
+    while(inputFile >> codeEntry)
+    {
+        //while input file is taking in some form of inpuit
+        //we will need t process teh data within the text file
+        if(inputFile.peek() == '!')
+        {
+            //this should remove any line that uses! at the beginning,
+            //it is a comment operator/indicator
+            getline(inputFile,commentTrash);
+        }
+        //take the data into the data containers we will use for manipulation
+        GlobalStorage.push_back(codeEntry);
 
-	inputFile.close();
+    }
 
-for(int idx = 0; index < GlobalStorage.size();idx++){
-	//we begin splitting strings right here
-	string currentLexeme =	GlobalStorage[idx];		
-	//a respective lexeme should now be held inside this vector of chars
-	//ex:  n, u , m , 1 , ","
-	TempVector(currentLexeme.begin(), currentLexeme.end());
-	vector<char> word;	
-	for(int charIdx = 0; charIdx < TempVector.size(); charIdx++){
-		//if the next item in the vector turns out not to be 
-		//human language char then, we will take all previous items and convert
-		//them to a singular string , and push it onto the vector, after that
-		//we will push that singular object as a singular respective token 
-		//into the vector
-		
-		if(isalnum(TempVector[charIdx]) == false){
-			
-			if(TempVector[charIdx] == '$'){
-				word.push_back(TempVector[charIdx]);	
-			}
-		VectorOfTokens.push_back(TempVector[charIdx]);
-		}	
-		word.push_back(TempVector[charIdx]);
-	}
-	VectorofTokens.push_back(word);
-	word.clear();
-}
-	
+    inputFile.close();
+
+    for(int idx = 0; idx < GlobalStorage.size(); idx++)
+    {
+        //we begin splitting strings right here
+        string currentLexeme =	GlobalStorage[idx];
+        //a respective lexeme should now be held inside this vector of chars
+        //ex:  n, u , m , 1 , ","
+        TempVector(currentLexeme.begin(), currentLexeme.end());
+        vector<char> word;
+        for(int charIdx = 0; charIdx < TempVector.size(); charIdx++)
+        {
+            //if the next item in the vector turns out not to be
+            //human language char then, we will take all previous items and convert
+            //them to a singular string , andpush it onto the vector, after that
+            //we will push that singular object as a singular respective token
+            //into the vector
+
+            if(isalnum(TempVector[charIdx]) == false)
+            {
+
+                if(TempVector[charIdx] == '$')
+                {
+                    word.push_back(TempVector[charIdx]);
+                }
+                VectorOfTokens.push_back(word);
+                VectorOfTokens.push_back(TempVector[charIdx]);
+            }
+            word.push_back(TempVector[charIdx]);
+        }
+        VectorofTokens.push_back(word);
+        word.clear();
+    }
+
 }
 
 
